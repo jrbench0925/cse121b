1a2bc5673a562db9e637db30841d7b1dab75909a
jrbench0925 <ben07010@byui.edu>
2024-01-14 18:31:41 -0700

    week2 image fix

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -5,7 +5,7 @@
 /* Step 2 - Variables */
 const fullName = "Jeff Bench";
 const currentYear = new Date().getFullYear;
-const profilePicture = images / profile.jpg;
+const profilePicture = "images/profile.jpg";
 
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById("name");
